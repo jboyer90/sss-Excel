5a620c8772cede2a315d975c5ed7dad18c57e375
Boyer <John.R.Boyer@ehi.com>
2020-04-30 12:57:08 -0500

    updated

--- src/main/java/com/sss/app/App.java
+++ src/main/java/com/sss/app/App.java
@@ -5,14 +5,14 @@ import com.sss.write.writeExcel;
 import java.io.IOException;
 
 public class App {
-    public static void main(String[] args) throws IOException {
-        //writeExcel write = new writeExcel();
-    String path = "C:\\Users\\E451Q1\\Desktop\\SSS Holiday Generator\\holiday generated.xlsx";
+   public static void main(String[] args) throws IOException {
+        writeExcel write = new writeExcel();
+    String path = "C:\\Users\\E451Q1\\Desktop\\SSS Holiday Generator\\sssExcel.xlsx";
         //write.writeSingleCellData(path);
-       // write.writeMultipleCellData(path);
+       write.writeMultipleCellData(path);
         //System.out.println("File Created!");
-        readExcel read = new readExcel();
+        //readExcel read = new readExcel();
         //read.readExcelDataForSingleRow(path);
-        read.readExcelDataForEntireSheet(path);
+        //read.readExcelDataForEntireSheet(path);
     }
 }
--- src/main/java/com/sss/read/readExcel.java
+++ src/main/java/com/sss/read/readExcel.java
@@ -1,81 +1,58 @@
 package com.sss.read;
+
 import org.apache.poi.EncryptedDocumentException;
 import org.apache.poi.ss.usermodel.*;
-
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import java.io.*;
 import java.util.Iterator;
 
 public class readExcel {
 
-    public void readExcelDataForSingleRow(String path) throws IOException {
-        // get workbook object
-        File file = new File(path);
-        try {
-            FileInputStream fis = new FileInputStream(file);
+    public void readExcelDataForEntireSheet(String path) throws EncryptedDocumentException {
 
+        try {
+            File f = new File(path);
+            new FileInputStream(f);
+            FileInputStream fis;
+            fis = new FileInputStream(f);
             //get workbook object from above stream
             Workbook workbook = WorkbookFactory.create(fis);
 
-            // get sheet from above
-            Sheet sheet = workbook.getSheetAt(0);
-
-            // get row from above
-            Row row = sheet.getRow(0);
-
-            // get the cell object
-            Cell cell = row.getCell(0);
-            System.out.println(cell.getStringCellValue());
-
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-    }
-    public void readExcelDataForEntireSheet(String path) throws EncryptedDocumentException, IOException {
-        File file = new File(path);
-        FileInputStream fis = null;
-        try {
-            fis = new FileInputStream(file);
-            //get workbook object from above stream
-            Workbook workbook = WorkbookFactory.create(fis);
 
             //iterate through the workbook
             Iterator<Sheet> sheetIterator = workbook.sheetIterator();
             while (sheetIterator.hasNext()){
                 Sheet sheet = sheetIterator.next();
-                // row iterator
-                Iterator<Row> rowIterator = sheet.iterator();
-                Row row = rowIterator.next();
-                Iterator<Cell> cellIterator = row.cellIterator();
 
-                while (cellIterator.hasNext()){
-                    Cell cell = cellIterator.next();
-                    switch (cell.getCellType()){
-                        case BLANK:
-                            System.out.print("" + "\t");
-                            break;
-                        case BOOLEAN:
-                            System.out.print(cell.getBooleanCellValue() + "\t");
-                            break;
-                        case ERROR:
-                            System.out.print("Error" + "\t");
-                            break;
-                        case FORMULA:
-                            System.out.print(cell.getCellFormula() + "\t");
-                            break;
-                        case STRING:
-                            System.out.print(cell.getStringCellValue() + "\t");
-                            break;
-                        case NUMERIC:
-                            System.out.print(cell.getNumericCellValue() + "\t");
-                            break;
-                        case _NONE:
-                            System.out.print("None" + "\t");
-                            break;
-                        default:
-                            break;
+                // For loop for role;
+
+                for (Row myRow : sheet) {
+                    for (Cell cell : myRow) {
+
+                        switch (cell.getCellType()){
+                            case BLANK:
+                                System.out.print("Blank" + "\t");
+                                break;
+                            case BOOLEAN:
+                                System.out.print(cell.getBooleanCellValue() + "\t");
+                                break;
+                            case ERROR:
+                                System.out.print(cell.getErrorCellValue() + "\t");
+                                break;
+                            case FORMULA:
+                                System.out.print(cell.getCellFormula() + "\t");
+                                break;
+                            case STRING:
+                                System.out.print(cell.getStringCellValue() + "\t");
+                                break;
+                            case NUMERIC:
+                                System.out.print(cell.getNumericCellValue() + "\t");
+                                break;
+                            case _NONE:
+                                System.out.print("None" + "\t");
+                                break;
+                            default:
+                                break;
+                        }
                     }
                 }
                 System.out.println();
@@ -86,3 +63,4 @@ public class readExcel {
         }
     }
 }
+
